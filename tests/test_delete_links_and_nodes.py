34ba257ae4d6de08f9f534f4deac319069f72285
Renata Imai <renataakemii@yahoo.co.uk>
2022-09-15 19:28:23 -0300

    delete links outside borders

---
+++ tests/test_delete_links_and_nodes.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestDeleteLinksAndNodes(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    @unittest.skip
+    def test_delete_links_and_nodes(self):
+        pass
+
+
+if __name__ == "__name__":
+    unittest.main()
---
+++ tradesman/model_creation/delete_links_and_nodes.py
@@ -0,0 +1,85 @@
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+from aequilibrae.project import Project
+from shapely.geometry import Polygon
+from os.path import join, isfile
+from tempfile import gettempdir
+from urllib.request import urlretrieve
+import pycountry
+from aequilibrae.project.project_creation import remove_triggers, add_triggers
+
+
+def get_maritime_boundaries(model_place: str):
+    """
+    Import maritime boundaries from countries. If country has no maritime boundary, it returns None.
+    Parameters:
+         *model_place*(:obj:`str`):
+    """
+    country_code = pycountry.countries.search_fuzzy(model_place)[0].alpha_3
+
+    url = "https://github.com/AequilibraE/tradesman/releases/download/V0.1b/maritime_boundaries.gpkg"
+
+    dest_path = join(gettempdir(), "maritime_boundaries.gpkg")
+
+    if not isfile(dest_path):
+        urlretrieve(url, dest_path)
+
+    gdf = gpd.read_file(dest_path)
+
+    if country_code in gdf.ISO_TER1.values:
+        return gdf[gdf.ISO_TER1 == country_code]
+    return
+
+
+def merge_country_borders(model_place, project: Project):
+    """
+    Removes links and nodes outside the political subdivision.
+    Parameters:
+         *model_place*(:obj:`str`):
+         *project*(:obj:`aequilibrae.project.Project`): currently open project
+    """
+
+    coast = get_maritime_boundaries(model_place)
+
+    sql = "SELECT country_name, division_name, level, Hex(ST_AsBinary(GEOMETRY)) geometry FROM political_subdivisions WHERE level=0;"
+
+    borders = gpd.GeoDataFrame.from_postgis(sql, project.conn, geom_col="geometry", crs=4326).explode(index_parts=True)
+
+    if coast is None:
+        gdf_country_boundary = borders.copy()
+
+    else:
+        coast = coast.explode(index_parts=True)
+
+        exploded_gdf = coast.overlay(borders, how="union").dissolve().explode(index_parts=True)
+
+        get_border_linearring = pd.DataFrame(
+            [Polygon(exploded_gdf.exterior.values[i]) for i in range(len(exploded_gdf))], columns=["geom"]
+        )
+
+        gdf_country_boundary = gpd.GeoDataFrame(get_border_linearring, geometry=get_border_linearring.geom, crs=4326)
+
+    links_query = "SELECT link_id, Hex(ST_AsBinary(GEOMETRY)) geometry FROM links;"
+
+    links = gpd.GeoDataFrame.from_postgis(links_query, project.conn, geom_col="geometry", crs=4326)
+
+    inner_gdf = gpd.sjoin(gdf_country_boundary, links, how="inner")
+
+    del_links = list(links[~links.link_id.isin(inner_gdf.link_id)][["link_id"]].itertuples(index=False, name=None))
+
+    remove_triggers(project.conn, project.logger)
+
+    project.conn.executemany("DELETE FROM links WHERE link_id=?", del_links)
+    project.conn.commit()
+
+    nodes_query = """DELETE FROM nodes
+    WHERE node_id NOT IN (SELECT a_node FROM links
+                        UNION ALL
+                                        SELECT b_node FROM links);
+    """
+
+    project.conn.execute(nodes_query)
+    project.conn.commit()
+
+    add_triggers(project.conn, project.logger)
