78a543e10cbaee9dbe037416abd309ad3dfa2027
Renata Imai <renataakemii@yahoo.co.uk>
2022-10-03 17:22:56 -0300

    Update test_generic_tag.py

--- tests/data_retrieval/test_generic_tag.py
+++ tests/data_retrieval/test_generic_tag.py
@@ -3,20 +3,21 @@ from tempfile import gettempdir
 import unittest
 from aequilibrae.project import Project
 from uuid import uuid4
-from shutil import copytree
+from shutil import copytree, rmtree
 from tradesman.data_retrieval.osm_tags.generic_tag import generic_tag
 
 
 class TestGenericTag(unittest.TestCase):
     def setUp(self) -> None:
         self.fldr = join(gettempdir(), uuid4().hex)
-        copytree(src=join(abspath(dirname("tests")), "tests/data/vatican city"), dst=self.fldr)
+        copytree(src=join(abspath(dirname("tests")), "data/vatican city"), dst=self.fldr)
         self.project = Project()
         self.project.open(self.fldr)
         self.osm_data = {}
 
     def tearDown(self) -> None:
         self.project.close()
+        rmtree(self.fldr)
 
     def test_generic_tag_amenity(self):
 
