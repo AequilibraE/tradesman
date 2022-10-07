f47deec7f12e5cece8fee48410f30cda6e21b7d9
Renata Imai <renataakemii@yahoo.co.uk>
2022-10-07 16:41:41 -0300

    Update test_generic_tag.py

--- tests/data_retrieval/test_generic_tag.py
+++ tests/data_retrieval/test_generic_tag.py
@@ -2,12 +2,13 @@
 from os.path import join
 from tempfile import gettempdir
 import unittest
-
-# from aequilibrae.project import Project
+from unittest import mock
 from uuid import uuid4
 from shutil import rmtree
+
 from aequilibrae.utils.create_example import create_example
 from tradesman.data_retrieval.osm_tags.generic_tag import generic_tag
+
 from tradesman.model_creation.add_country_borders import add_country_borders_to_model
 from tradesman.model_creation.create_new_tables import add_new_tables
 
@@ -15,28 +16,24 @@ from tradesman.model_creation.create_new_tables import add_new_tables
 class TestGenericTag(unittest.TestCase):
     def setUp(self) -> None:
         self.fldr = join(gettempdir(), uuid4().hex)
-        # copytree(src=join(abspath(dirname("tests")), "data/vatican city"), dst=self.fldr)
+        # copytree(src=join(abspath(dirname("tests")), "data/vatican city/project"), dst=self.fldr)
         self.project = create_example(self.fldr, "nauru")
-        self.osm_data = {}
         add_new_tables(self.project.conn)
         add_country_borders_to_model("Nauru", self.project)
+        self.osm_data = {"amenity": "fooboo", "building": "boofoo"}
+        self.patcher = mock.patch("tradesman.data_retrieval.osm_tags.generic_tag.load_osm_data")
+        self.patcher.start()
 
     def tearDown(self) -> None:
+        self.patcher.stop()
         rmtree(self.fldr, ignore_errors=True)
 
-    @unittest.skip
     def test_generic_tag_amenity(self):
+        self.assertEqual(generic_tag(tag="amenity", project=self.project, osm_data=self.osm_data), "fooboo")
 
-        self.assertEqual(
-            type(generic_tag(tag="amenity", osm_data=self.osm_data, tile_size=25, project=self.project)), list
-        )
-
-    @unittest.skip
     def test_generic_tag_building(self):
 
-        self.assertEqual(
-            type(generic_tag(tag="building", osm_data=self.osm_data, tile_size=25, project=self.project)), list
-        )
+        self.assertEqual(generic_tag(tag="building", osm_data=self.osm_data, project=self.project), "boofoo")
 
 
 if __name__ == "__name__":
