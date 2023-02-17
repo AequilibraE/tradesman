41d23bb658497cd1d8aea85d8055c581eef12d6a
Renata Imai <renataakemii@yahoo.co.uk>
2023-02-17 13:48:08 -0300

    Update mask_raster.py
    Avoids using multiplygons to create the mask

--- tradesman/data/mask_raster.py
+++ tradesman/data/mask_raster.py
@@ -1,12 +1,15 @@
-import os
-from tempfile import gettempdir
 import rasterio.mask
 import rasterio
+import geopandas as gpd
+from shapely.geometry import box
 
 
 def mask_raster(dest_path, main_area):
+    """
+    This function was presented in: https://rasterio.readthedocs.io/en/latest/topics/masking-by-shapefile.html
+    """
     with rasterio.open(dest_path) as src:
-        out_img, out_transform = rasterio.mask.mask(src, main_area, crop=True)
+        out_img, out_transform = rasterio.mask.mask(src, areaGeoJson(main_area), crop=True)
         out_meta = src.meta
 
     out_meta.update(
@@ -17,3 +20,14 @@ def mask_raster(dest_path, main_area):
         dest.write(out_img)
 
     return rasterio.open(dest_path)
+
+
+def areaGeoJson(main_area):
+    """
+    Adapted from the function and code created by Henrikki Tenkanen.
+    See: https://automating-gis-processes.github.io/CSC18/lessons/L6/clipping-raster.html
+    """
+    import json
+
+    geo = gpd.GeoDataFrame({"geometry": box(*main_area.bounds)}, index=[0], crs=4326)
+    return [json.loads(geo.to_json())["features"][0]["geometry"]]
