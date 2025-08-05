98120245571c21e1d023588aa62a64d01777a9f3
Renata Imai <renataakemii@yahoo.co.uk>
2025-08-05 17:49:27 -0300

    version and connections

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,20 +22,23 @@ def empty_aequilibrae_model(create_path):
     prj = Project()
     prj.new(create_path)
 
-    add_new_tables(prj.conn)
+    with prj.db_connection as conn:
+        add_new_tables(conn)
     yield prj
     prj.close()
 
 
 @pytest.fixture
 def network_connection(empty_aequilibrae_model):
-    yield empty_aequilibrae_model.conn
+    with empty_aequilibrae_model.db_connection as conn:
+        yield conn
 
 
 @pytest.fixture
 def nauru_test(create_path, tmp_path):
     prj = create_example(create_path, "nauru")
-    add_new_tables(prj.conn)
+    with prj.db_connection as conn:
+        add_new_tables(conn)
 
     shutil.copy(
         os.path.join(os.path.dirname(__file__), "data/nauru/Nauru_cache_gadm.parquet"),
@@ -52,7 +55,7 @@ def nauru_test(create_path, tmp_path):
 
 
 @pytest.fixture
-def nauru_pop_test(nauru_test, create_path, tmp_path):
+def nauru_pop_test(nauru_test, tmp_path):
     shutil.copy(
         os.path.join(os.path.dirname(__file__), "data/nauru/pop_Nauru.tif"),
         os.path.join(tmp_path, "pop_Nauru.tif"),
@@ -69,8 +72,8 @@ def nauru_pop_test(nauru_test, create_path, tmp_path):
 
     select_pop = gdf.clip(model_area, keep_geom_type=True)[["longitude", "latitude", "population"]]
 
-    select_pop.to_sql("raw_population", nauru_test.conn, if_exists="append", index=False)
-    nauru_test.conn.execute("UPDATE raw_population SET Geometry=MakePoint(longitude, latitude, 4326)")
-    nauru_test.conn.commit()
+    with nauru_test.db_connection as conn:
+        select_pop.to_sql("raw_population", conn, if_exists="append", index=False)
+        conn.execute("UPDATE raw_population SET Geometry=MakePoint(longitude, latitude, 4326)")
 
     yield nauru_test
--- tests/create_nauru_test.py
+++ tests/create_nauru_test.py
@@ -17,65 +17,62 @@ def create_nauru_test(folder):
     df["geom"] = gpd.GeoSeries.to_wkb(df["geometry"])
 
     project = create_example(folder, "nauru")
-    add_new_tables(project.conn)
+    with project.db_connection as conn:
+        add_new_tables(conn)
 
-    data = ImportPoliticalSubdivisions(model_place="Nauru", project=project, source="GADM")
-    data.import_model_area()
-    data.add_country_borders(overwrite=True)
+        data = ImportPoliticalSubdivisions(model_place="Nauru", project=project, source="GADM")
+        data.import_model_area()
+        data.add_country_borders(overwrite=True)
 
-    zones = 12
+        zones = 12
 
-    network = project.network
+        network = project.network
 
-    geo = network.convex_hull()
+        geo = network.convex_hull()
 
-    zone_area = geo.area / zones
+        zone_area = geo.area / zones
 
-    zone_side = sqrt(2 * sqrt(3) * zone_area / 9)
+        zone_side = sqrt(2 * sqrt(3) * zone_area / 9)
 
-    extent = network.extent()
+        extent = network.extent()
 
-    curr = project.conn.cursor()
-    b = extent.bounds
-    curr.execute(
-        "select st_asbinary(HexagonalGrid(GeomFromWKB(?), ?, 0, GeomFromWKB(?)))",
-        [extent.wkb, zone_side, Point(b[2], b[3]).wkb],
-    )
-    grid = curr.fetchone()[0]
-    grid = shapely.wkb.loads(grid)
+        b = extent.bounds
+        conn.execute(
+            "select st_asbinary(HexagonalGrid(GeomFromWKB(?), ?, 0, GeomFromWKB(?)))",
+            [extent.wkb, zone_side, Point(b[2], b[3]).wkb],
+        )
+        grid = conn.cursor().fetchone()[0]
+        grid = shapely.wkb.loads(grid)
 
-    grid = [p for p in grid.geoms if p.intersects(geo)]
+        grid = [p for p in grid.geoms if p.intersects(geo)]
 
-    nodes = network.nodes
-    for i in range(1, 301):
-        nd = nodes.get(i)
-        nd.renumber(i + 1300)
+        nodes = network.nodes
+        for i in range(1, 301):
+            nd = nodes.get(i)
+            nd.renumber(i + 1300)
 
-    zoning = project.zoning
-    for i, zone_geo in enumerate(grid):
-        zone = zoning.new(i + 1)
-        zone.geometry = zone_geo
-        zone.save()
+        zoning = project.zoning
+        for i, zone_geo in enumerate(grid):
+            zone = zoning.new(i + 1)
+            zone.geometry = zone_geo
+            zone.save()
 
-    zones_from_location = gpd.GeoDataFrame.from_postgis(
-        "SELECT zone_id,  Hex(ST_AsBinary(geometry)) as geom FROM zones;", project.conn, geom_col="geom", crs=4326
-    )
+        zones_from_location = gpd.GeoDataFrame.from_postgis(
+            "SELECT zone_id,  Hex(ST_AsBinary(geometry)) as geom FROM zones;", project.conn, geom_col="geom", crs=4326
+        )
 
-    population = [358, 686, 541, 966, 474, 158, 1088, 214, 1178, 473, 59, 1004, 186, 499, 331, 199, 763, 447, 222]
+        population = [358, 686, 541, 966, 474, 158, 1088, 214, 1178, 473, 59, 1004, 186, 499, 331, 199, 763, 447, 222]
 
-    qry_values = list(zip(population, zones_from_location.zone_id.values))
+        qry_values = list(zip(population, zones_from_location.zone_id.values))
 
-    project.conn.executemany("UPDATE zones SET population=? WHERE zone_id=?;", qry_values)
-    project.conn.commit()
+        conn.executemany("UPDATE zones SET population=? WHERE zone_id=?;", qry_values)
 
-    project.conn.execute("DELETE FROM political_subdivisions WHERE level>0;")
-    project.conn.commit()
+        conn.execute("DELETE FROM political_subdivisions WHERE level>0;")
 
-    qry = "INSERT INTO political_subdivisions (country_name, division_name, level, geometry) \
-            VALUES(?, ?, ?, CastToMulti(GeomFromWKB(?, 4326)));"
-    list_of_tuples = list(df[["country_name", "division_name", "level", "geom"]].itertuples(index=False, name=None))
+        qry = "INSERT INTO political_subdivisions (country_name, division_name, level, geometry) \
+                VALUES(?, ?, ?, CastToMulti(GeomFromWKB(?, 4326)));"
+        list_of_tuples = list(df[["country_name", "division_name", "level", "geom"]].itertuples(index=False, name=None))
 
-    project.conn.executemany(qry, list_of_tuples)
-    project.conn.commit()
+        conn.executemany(qry, list_of_tuples)
 
     return project
